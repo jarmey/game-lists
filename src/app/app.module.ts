55e327f94f3f4d91db9525db7fdc662caa8aaebf
Joel Armey <joel.armey@realpage.com>
2019-07-05 15:09:16 -0500

    Add games component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListsComponent } from './lists/lists.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListsComponent
+    ListsComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
