fcd2413a5c55aaca970d27df0712b2cc88e5087d
Joel Armey <joel.armey@realpage.com>
2019-07-05 15:04:04 -0500

    Add lists component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListsComponent } from './lists/lists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
